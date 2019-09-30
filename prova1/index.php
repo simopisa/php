ecfe7cdf64bdd6c678ffe1c5cb14577923b233cd
SIMONE.PISONI <SIMONE.PISONI@buonarroti.tn.it>
2019-09-30 10:07:27 +0200

    inserimento esempi

--- prova1/index.php
+++ prova1/index.php
@@ -1,15 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>prova</title>
 </head>
+
 <body>
     <?php
-        echo "<h1>banan a</h1>";
+     //________________________________________________________________________________________________________________________-
+     $nomi=array("marco","luca");
+     foreach ($nomi as $valore) {
+         echo $valore."<br />";
+     }
+    $colori = array('bianco',"rosso","ciao" );
+    foreach ($colori as $colore) {
+        $colore=strtoupper($colore); // non modifico il valore dell'array
+        echo $colore ,"<br>";
+    }
+    foreach ($colori as $colore) {
+        echo $colore ,"<br>";
+    }
+    foreach ($colori as &$colore) {//cambia il contenuto dell'array con &
+        $colore=strtoupper($colore); 
+        echo $colore ,"<br>";
+    }
+    unset($colore);//cancella la variabile
+
+    foreach ($colori as $colore) {
+        echo $colore ,"<br>";
+    }
+    foreach ($colori as $key => $valore) {
+        echo $key ,"<br>", $valore, "<br>";
+    }
+    //________________________________________________________________________________________________________________________-
+     $film = array('titolo' => 'Via col Vento', 'anno' => 1939,'regista' => 'Victor Fleming');
+    foreach ($film as $info => $valore) {
+        echo "$info: $valore <br>\n"; 
+    }
+
+
+
+
+     //________________________________________________________________________________________________________________________-   
+      
     ?>
-    
+
 </body>
+
 </html>
